2d6112a9eba28390a2b385ead177ff80880d69d6
Javier Domingo Cansino <javierdo1@gmail.com>
2013-10-25 13:57:54 +0200

    Making the script first check the mpf file

--- make-zips.sh
+++ make-zips.sh
@@ -3,6 +3,11 @@ name=JAVIERD
 
 for project in `ls *[0-9]* -d`; do
 	cd $project
+
+	proj_file=`find . -name "*.mpf"`
+	proj_path=$(pwd | sed -e  's/\//\\\//g')
+	sed -ie "s/$proj_path\///g" *.mpf
+
 	files=`find . -name "*.vhd" -o -name "*.mpf" -o -name "*.png" -o -name "fichlectura.txt"| sed -e 's/^.*\///g'`
 	number=`echo $project | sed -e 's/[a-zA-Z_]*//g'`
 	zip -j $name-$number.zip $files
