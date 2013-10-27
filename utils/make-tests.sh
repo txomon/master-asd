6868dc3f14f534aab6dc7e13309ba0a67e7f3ddb
Javier Domingo Cansino <javierdo1@gmail.com>
2013-10-27 15:40:50 +0100

    ci: Created an script that executes tests

---
+++ utils/make-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $(git rev-parse --show-toplevel)
+
+ex_code=0
+for project in `ls *[0-9]* -d`; do
+	cd $project
+	vsim -c -do ../utils/test_project.tcl
+	ex_code=$(expr $ex_code + $?)
+	cd ..
+done;
+
+exit $ex_code
