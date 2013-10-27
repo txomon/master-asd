6252cf63d5b3727c310dba2dec570b84b21577e7
Javier Domingo Cansino <javierdo1@gmail.com>
2013-10-27 13:07:52 +0100

    Finished exercise 15

--- tb_ctrl_15/ctrl.vhd
+++ tb_ctrl_15/ctrl.vhd
@@ -2,4 +2,66 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY ctrl IS
-  PORT
+  PORT (
+    SIGNAL rst :IN STD_LOGIC;
+    SIGNAL clk :IN STD_LOGIC;
+    SIGNAL stb :IN STD_LOGIC;
+    SIGNAL m :IN STD_LOGIC;
+    SIGNAL tx :OUT STD_LOGIC;
+    SIGNAL ld :OUT STD_LOGIC;
+    SIGNAL en :OUT STD_LOGIC;
+    SIGNAL ini :OUT STD_LOGIC
+  );
+END ctrl;
+
+ARCHITECTURE behavioral OF ctrl IS
+  TYPE STATUS_TYPE IS (SBY, STA, B0, B1, B2, B3, B4, B5, B6, B7, STO);
+  SIGNAL status : STATUS_TYPE := SBY;
+BEGIN
+
+  -- Status progress
+  PROCESS(clk)
+    BEGIN
+      IF RISING_EDGE(clk) THEN
+        IF rst = '0' THEN
+          CASE status IS
+            WHEN SBY => IF stb = '1' THEN status <= STA; END IF;
+            WHEN STA => status <= B0;
+            WHEN B0 => status <= B1;
+            WHEN B1 => status <= B2;
+            WHEN B2 => status <= B3;
+            WHEN B3 => status <= B4;
+            WHEN B4 => status <= B5;
+            WHEN B5 => status <= B6;
+            WHEN B6 => status <= B7;
+            WHEN B7 => status <= STO;
+            WHEN STO => status <= SBY;
+          END CASE;
+        ELSE
+          status <= SBY;
+        END IF;
+      END IF;
+    END PROCESS;
+
+  -- TX out pin
+  WITH status SELECT
+  tx <= '0' WHEN STA,
+        '1' WHEN SBY | STO,
+        m WHEN OTHERS;
+
+  -- LD out pin
+  WITH status SELECT
+  ld <= '1' WHEN SBY,
+        '0' WHEN OTHERS;
+
+  -- EN out pin
+  WITH status SELECT
+  en <= '0' WHEN SBY | STA | B7 | STO,
+        '1' WHEN OTHERS;
+
+  -- INI out pin
+  WITH status SELECT
+  ini <= '1' WHEN SBY,
+         '0' WHEN OTHERS;
+
+END behavioral;
--- tb_ctrl_15/tb_ctrl.vhd
+++ tb_ctrl_15/tb_ctrl.vhd
@@ -1,1 +1,70 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 
+ENTITY tb_ctrl IS
+  PORT (
+    SIGNAL tx :OUT STD_LOGIC;
+    SIGNAL ld :OUT STD_LOGIC;
+    SIGNAL en :OUT STD_LOGIC;
+    SIGNAL ini :OUT STD_LOGIC
+  );
+END tb_ctrl;
+
+ARCHITECTURE structural OF tb_ctrl IS
+  COMPONENT rstclk IS
+    GENERIC (
+      CONSTANT trst :IN TIME := 320 ns;
+      CONSTANT tpck :IN TIME := 30 ns;
+      CONSTANT tclk :IN TIME := 100 ns;
+      CONSTANT ti1 :IN TIME := 5035 ns;
+      CONSTANT tf1 :IN TIME := 5815 ns;
+      CONSTANT ti2 :IN TIME := 3015 ns;
+      CONSTANT tf2 :IN TIME := 3415 ns
+    );
+
+    PORT   (
+      SIGNAL rst :OUT STD_LOGIC;
+      SIGNAL clk :OUT STD_LOGIC;
+      SIGNAL out1 :OUT STD_LOGIC;
+      SIGNAL out2 :OUT STD_LOGIC
+    );
+  END COMPONENT;
+
+  COMPONENT ctrl IS
+    PORT (
+      SIGNAL rst :IN STD_LOGIC;
+      SIGNAL clk :IN STD_LOGIC;
+      SIGNAL stb :IN STD_LOGIC;
+      SIGNAL m :IN STD_LOGIC;
+      SIGNAL tx :OUT STD_LOGIC;
+      SIGNAL ld :OUT STD_LOGIC;
+      SIGNAL en :OUT STD_LOGIC;
+      SIGNAL ini :OUT STD_LOGIC
+    );
+  END COMPONENT;
+
+  SIGNAL in_clk : STD_LOGIC;
+  SIGNAL in_rst : STD_LOGIC;
+  SIGNAL in_stb : STD_LOGIC;
+  SIGNAL in_m : STD_LOGIC;
+BEGIN
+  RST : rstclk
+    PORT MAP (
+      rst => in_rst,
+      clk => in_clk,
+      out1 => in_stb,
+      out2 => in_m
+    );
+
+  CTL : ctrl
+    PORT MAP (
+      rst => in_rst,
+      clk => in_clk,
+      stb => in_stb,
+      m => in_m,
+      tx => tx,
+      ld => ld,
+      en => en,
+      ini => ini
+    );
+END structural;
