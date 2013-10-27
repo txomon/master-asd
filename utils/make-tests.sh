07cea40b8ef49fa82908f82fb9af2a8ec8fe39bb
Javier Domingo Cansino <javierdo1@gmail.com>
2013-10-27 16:04:21 +0100

    ci: trimming output and trying to get real exit codes on Modelsim failures

--- utils/make-tests.sh
+++ utils/make-tests.sh
@@ -5,7 +5,7 @@ cd $(git rev-parse --show-toplevel)
 ex_code=0
 for project in `ls *[0-9]* -d`; do
 	cd $project
-	vsim -c -do ../utils/test_project.tcl
+	vsim -keepstdout -runinit -c -do ../utils/test_project.tcl
 	ex_code=$(expr $ex_code + $?)
 	cd ..
 done;
